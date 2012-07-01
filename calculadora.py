67487ccfc114599a52938f3b4bd9bce9048cb865
Alicia <izquierdogarcia@gmail.com>
2012-07-01 14:04:32 +0200

    Excepciones

--- calculadora.py
+++ calculadora.py
@@ -7,6 +7,7 @@ def list_to_int(l):
     return int("".join(str(x) for x in l))
 
 respuesta = 0
+numeros = []
 while (respuesta != '5'):
 	print 'CALCULADORA SENCILLA'
 	print 'Elige la operación a realizar:'
@@ -14,43 +15,48 @@ while (respuesta != '5'):
 	print '2. Restar'
 	print '3. Multiplicar'
 	print '4. Dividir'
-	print '5. SALIR'
+	print '5. SALIR'	
 
-	respuesta = raw_input('Opción: ')
-	if respuesta == '5': 
-		break
-
-
-	
-		numeros = raw_input('Introduce los dígitos separados por un espacio: ')
-		numeros = numeros.split(' ')
-		list_to_int(numeros)
-
-	n = 0
-	if respuesta > 0 and respuesta < 5:
-		for resultado in numeros:
-			if respuesta == '1':
-			    n = n + int(resultado)
-			elif respuesta == '2': 
-				if n == 0:
-				    n = int(resultado)
-				else:
-					n = n - int(resultado)
-			elif respuesta == '3':
-				if n == 0:
-				    n = int(resultado)
-				else:
-					n = n * int(resultado)
-			elif respuesta == '4':
-			    if n == 0:
-				    n = int(resultado)
-			    else:
-				    n = n / int(resultado)
-			
-
-		print n
+	try:
+	    respuesta = int(raw_input('Opción: '))
+	except:
+		print "No es un número"
 	else:
-		print "Elija la opción correcta."
+		if respuesta == 5: 
+			break	
+
+	    
+		n = 0
+		if respuesta > 0 and respuesta < 5:
+			numeros = raw_input('Introduce los dígitos separados por un espacio: ')
+			numeros = numeros.split(' ')
+	        try:
+	            list_to_int(numeros)
+	        except:
+		        print "No son números2."
+	        else:
+			    for resultado in numeros:
+					if respuesta == 1:
+					    n = n + int(resultado)
+					elif respuesta == 2: 
+						if n == 0:
+						    n = int(resultado)
+						else:
+							n = n - int(resultado)
+					elif respuesta == 3:
+						if n == 0:
+						    n = int(resultado)
+						else:
+							n = n * int(resultado)
+					elif respuesta == 4:
+					    if n == 0:
+						    n = int(resultado)
+					    else:
+						    n = n / int(resultado)
+					
+
+			    print n
+	
 
 
 
